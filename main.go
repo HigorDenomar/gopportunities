41373ab2aa9a86892a9ce523d2e4c005db658913
Higor Denomar <higordenomar@gmail.com>
2023-09-04 20:12:21 -0300

    feat: create routes with gin

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/higordenomar/gopportunities/router"
+
+func main() {
+	router.Init("localhost:8080")
+}
---
+++ router/routes.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/higordenomar/gopportunities/handler"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1")
+
+	{
+		v1.POST("/opportunity", handler.CreateOpportunityHandler)
+
+		v1.GET("/opportunity", handler.ShowOpportunityHandler)
+
+		v1.GET("/opportunities", handler.ListOpportunitiesHandler)
+
+		v1.PUT("/opportunity", handler.UpdateOpportunityHandler)
+
+		v1.DELETE("/opportunity", handler.DeleteOpportunityHandler)
+	}
+}
