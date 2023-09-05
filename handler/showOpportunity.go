234bc137d0a4afc2724a62195ac7aeea65038a9b
Higor Denomar <higordenomar@gmail.com>
2023-09-05 17:19:20 -0300

    feat: show opportunity route

--- handler/showOpportunity.go
+++ handler/showOpportunity.go
@@ -4,10 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/higordenomar/gopportunities/schemas"
 )
 
 func ShowOpportunityHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "SHOW Opportunity",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opportunity := schemas.Opportunity{}
+
+	if err := db.First(&opportunity, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opportunity not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-opportunity", opportunity)
 }
