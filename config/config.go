9813c138d34cffcd93a0f607de76ce4fabe475b1
Higor Denomar <higordenomar@gmail.com>
2023-09-04 21:02:53 -0300

    feat: create custom logger package

---
+++ config/config.go
@@ -0,0 +1,15 @@
+package config
+
+var (
+	logger *Logger
+)
+
+func Init() error {
+	return nil
+}
+
+func GetLogger(prefix string) *Logger {
+	logger = NewLogger(prefix)
+
+	return logger
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/higordenomar/gopportunities/router"
+import (
+	"github.com/higordenomar/gopportunities/config"
+	"github.com/higordenomar/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	err := config.Init()
+	logger = config.GetLogger("main")
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Init("localhost:8080")
 }
