5a2395b9b457f08d8326eb8705c30892ed92ad08
Higor Denomar <higordenomar@gmail.com>
2023-09-05 17:12:03 -0300

    feat: list opportunities route

--- handler/listOpportunities.go
+++ handler/listOpportunities.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/higordenomar/gopportunities/schemas"
 )
 
 func ListOpportunitiesHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "LIST Opportunity",
-	})
+	opportunities := []schemas.Opportunity{}
+
+	if err := db.Find(&opportunities).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing opportunities")
+		return
+	}
+
+	sendSuccess(ctx, "list-opportunities", opportunities)
 }
